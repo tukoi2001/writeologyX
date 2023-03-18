8e5a1219098e17ab32b2fcd3e6c853e4b7033bb9
hung_nm <hung.nm@hodfords.com>
2023-03-18 16:53:40 +0700

    [Feat] Create lastest ariticles components

---
+++ src/containers/LatestArticles/ArticleItem.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="article-item">
+    <div class="article-item__image">
+      <img :src="props.item.linkImagePost" />
+    </div>
+    <div class="article-item__content">
+      <div class="article-item__content-category mb-4">
+        {{ props.item.category }}
+      </div>
+      <div class="article-item__content-title mb-2">
+        {{ props.item.title }}
+      </div>
+      <div class="article-item__content-subtitle mb-5">
+        {{ props.item.subtitle }}
+      </div>
+      <div class="article-item__content-author">
+        <div class="article-item__author">
+          <v-avatar size="small" class="mr-2">
+            <v-img :src="props.item.author.image" />
+          </v-avatar>
+          {{ props.item.author.name }}
+        </div>
+        <div class="article-item__author">
+          <font-icon
+            class="article-item__date-icon mr-2"
+            size="30"
+            name="mdi-clock-outline"
+          />
+          {{ props.item.createdAt }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, type PropType } from 'vue';
+
+type LatestArticles = {
+  id: string;
+  category: string;
+  title: string;
+  subtitle: string;
+  author: {
+    id: string;
+    name: string;
+    image: string;
+  };
+  linkImagePost: string;
+  createdAt: string;
+};
+
+export default defineComponent({
+  name: 'ArticleItem',
+  components: {},
+  props: {
+    item: {
+      type: Object as PropType<LatestArticles>,
+      required: true,
+    },
+  },
+  setup(props) {
+    return {
+      props,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.article-item {
+  @include flex-box();
+  gap: 32px;
+
+  &__image {
+    width: 314px;
+    height: 314px;
+    overflow: hidden;
+    border-radius: 12px;
+    object-fit: cover;
+
+    img {
+      max-width: 100%;
+    }
+  }
+
+  &__content {
+    &-category {
+      @include sub-heading-text();
+    }
+
+    &-title {
+      @include heading-text();
+    }
+
+    &-subtitle {
+      @include sub-title-text();
+    }
+
+    &-author {
+      @include flex-box(flex-start, center);
+      gap: 30px;
+    }
+  }
+
+  &__author {
+    @include flex-box();
+    @include text-style(16px, 700, 18px, $black-1);
+  }
+}
+</style>
